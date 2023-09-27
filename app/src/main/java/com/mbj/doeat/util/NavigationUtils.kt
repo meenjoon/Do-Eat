57823a6554a335b39af8339da73520e581eb9eb8
meenjoon <zqudwnsz@naver.com>
2023-09-27 18:06:24 +0900

    feat: 네비게이션의 다양한 이동 로직을 효율적으로 관리하기 위한 함수화

---
+++ app/src/main/java/com/mbj/doeat/util/NavigationUtils.kt
@@ -0,0 +1,21 @@
+package com.mbj.doeat.util
+
+import androidx.navigation.NavHostController
+
+object NavigationUtils {
+    fun navigate(
+        controller: NavHostController,
+        routeName: String,
+        backStackRouteName: String? = null,
+        isLaunchSingleTop: Boolean = true,
+        needToRestoreState: Boolean = true
+    ) {
+        controller.navigate(routeName) {
+            if (backStackRouteName != null) {
+                popUpTo(backStackRouteName) { saveState = true }
+            }
+            launchSingleTop = isLaunchSingleTop
+            restoreState = needToRestoreState
+        }
+    }
+}
