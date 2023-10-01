e214cd2d04b0cd8d461289eed6cc542c6ab8ddd4
meenjoon <zqudwnsz@naver.com>
2023-10-01 20:07:20 +0900

    feat: SearchService(네이버 검색) 구현

---
+++ app/src/main/java/com/mbj/doeat/data/remote/network/service/SearchService.kt
@@ -0,0 +1,15 @@
+package com.mbj.doeat.data.remote.network.service
+
+import com.mbj.doeat.data.remote.model.SearchResult
+import com.mbj.doeat.data.remote.network.adapter.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SearchService {
+
+    @GET("v1/search/local.json")
+    suspend fun getFamousRestaurant(
+        @Query("query") query: String,
+        @Query("display") display: Int
+    ): ApiResponse<SearchResult>
+}
