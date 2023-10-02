5e98491f0c8c8941afbab5e445519255878f23af
meenjoon <zqudwnsz@naver.com>
2023-10-02 14:35:51 +0900

    feat: RoundedLine 컴포넌트 구현

---
+++ app/src/main/java/com/mbj/doeat/ui/component/RoundedLine.kt
@@ -0,0 +1,27 @@
+package com.mbj.doeat.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun RoundedLine(
+    strokeWidth: Dp,
+    cornerRadius: Dp,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(strokeWidth)
+            .clip(RoundedCornerShape(cornerRadius))
+            .background(Color.Black)
+    )
+}
