058ac8bace6580df92fb18b8d680d12275ee9031
meenjoon <zqudwnsz@naver.com>
2023-10-11 22:06:58 +0900

    feat: PartyItem 컴포넌트 구현

--- app/src/main/java/com/mbj/doeat/ui/screen/home/detail/DetailScreen.kt
+++ app/src/main/java/com/mbj/doeat/ui/screen/home/detail/DetailScreen.kt
@@ -1,21 +1,33 @@
 package com.mbj.doeat.ui.screen.home.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.accompanist.web.AccompanistWebChromeClient
 import com.google.accompanist.web.AccompanistWebViewClient
@@ -23,8 +35,12 @@ import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewNavigator
 import com.google.accompanist.web.rememberWebViewState
 import com.mbj.doeat.BuildConfig
+import com.mbj.doeat.data.remote.model.Party
 import com.mbj.doeat.data.remote.model.SearchItem
 import com.mbj.doeat.ui.screen.home.detail.viewmodel.DetailViewModel
+import com.mbj.doeat.ui.theme.Beige100
+import com.mbj.doeat.ui.theme.DoEatTheme
+import com.mbj.doeat.ui.theme.Remon400
 import com.mbj.doeat.util.UrlUtils
 
 @Composable
@@ -86,9 +102,125 @@ fun DetailScreen(searchItem: SearchItem, navController: NavHostController, onCli
                 }
             },
             modifier = Modifier
-                .fillMaxHeight(0.6f)
+                .fillMaxHeight(0.5f)
                 .fillMaxWidth()
                 .padding(8.dp)
         )
     }
-}
+
+    @Composable
+    fun PartyItem(
+        party: Party,
+        onJoinClick: (Party) -> Unit
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .background(Beige100, shape = RoundedCornerShape(12.dp))
+                .border(1.dp, Color.Gray, shape = RoundedCornerShape(12.dp))
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = party.restaurantName,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = Color.Black,
+                        modifier = Modifier
+                            .fillMaxWidth(0.6f)
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Text(
+                        text = party.category,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 12.sp,
+                        color = Color.Gray,
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+
+                Text(
+                    text = party.restaurantLocation,
+                    fontSize = 11.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(top = 4.dp)
+
+                )
+
+                Text(
+                    text = party.detail,
+                    fontSize = 14.sp,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp)
+                        .clickable {
+                            onJoinClick(party)
+                        },
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "${party.currentNumberPeople} / ${party.recruitmentLimit}",
+                        fontSize = 22.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                    )
+
+                    Spacer(modifier = Modifier.width(40.dp))
+
+                    Text(
+                        text = "참가하기",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier
+                            .background(Remon400, shape = RoundedCornerShape(4.dp))
+                            .padding(4.dp)
+                            .clickable {
+                                /**
+                                 * 채팅방 참가하기 TODO
+                                 */
+                                onJoinClick
+                            }
+                    )
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PartyItemPreview() {
+        DoEatTheme {
+            val dummyParty = Party(
+                postId = 11,
+                userId = 4,
+                restaurantName = "마노디셰프1231231212231231313131313123",
+                restaurantLocation = "서울특별시 송파구 송파대로 570 타워 730 B1",
+                recruitmentLimit = 10,
+                currentNumberPeople = 5,
+                detail = "This is a party description. This is a party description. This is a party description. This is a party description. This is a party description. This is a party description.",
+                link = "",
+                category = "음식점>이탈리아 음식 12313123131312312"
+            )
+            PartyItem(party = dummyParty, onJoinClick = {})
+        }
+    }
--- app/src/main/java/com/mbj/doeat/ui/theme/Color.kt
+++ app/src/main/java/com/mbj/doeat/ui/theme/Color.kt
@@ -8,6 +8,8 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Yellow700 = Color(0xFFFFEB23)
 val Gray200 = Color(0xFFB9B6B6)
+val Beige100 = Color(0xFFEBDFDF)
+val Remon400 = Color(0xFFF4FF7F)
 
 val randomColors = listOf(
     Color.Red,
