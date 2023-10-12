e40377ffac05545aed5fd3ac424438c5cd902372
meenjoon <zqudwnsz@naver.com>
2023-10-12 18:10:28 +0900

    feat: YesNoDialog 컴포넌트 구현

--- app/src/main/java/com/mbj/doeat/ui/theme/Color.kt
+++ app/src/main/java/com/mbj/doeat/ui/theme/Color.kt
@@ -8,8 +8,10 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Yellow700 = Color(0xFFFFEB23)
 val Gray200 = Color(0xFFB9B6B6)
+val Beige50 = Color(0xFFFFF1F1)
 val Beige100 = Color(0xFFEBDFDF)
 val Remon400 = Color(0xFFF4FF7F)
+val Pink500 = Color(0xFFFF7F7F)
 
 val randomColors = listOf(
     Color.Red,
