593e6cdd738161ce543ad673397b5c8b5812f5fb
meenjoon <zqudwnsz@naver.com>
2023-10-13 11:23:25 +0900

    feat: LoadingView 컴포넌트 구현

---
+++ app/src/main/java/com/mbj/doeat/ui/component/LadingView.kt
@@ -0,0 +1,29 @@
+package com.mbj.doeat.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.mbj.doeat.ui.theme.Black700
+
+@Composable
+fun LoadingView(
+    isLoading: Boolean,
+    boxColor: Color = Black700,
+    circularProgressIndicatorColor: Color = Color.White
+) {
+    if (isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(boxColor),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(color = circularProgressIndicatorColor)
+        }
+    }
+}
