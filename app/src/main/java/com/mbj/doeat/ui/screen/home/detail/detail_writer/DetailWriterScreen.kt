242f4037731c6546db58c367bbd1ce356955ae79
meenjoon <zqudwnsz@naver.com>
2023-10-15 15:44:48 +0900

    feat: DetailWriterScreen 생성

---
+++ app/src/main/java/com/mbj/doeat/ui/screen/home/detail/detail_writer/DetailWriterScreen.kt
@@ -0,0 +1,9 @@
+package com.mbj.doeat.ui.screen.home.detail.detail_writer
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.mbj.doeat.data.remote.model.Party
+
+@Composable
+fun DetailWriterScreen(party: Party, navController: NavHostController, onClick: () -> Unit) {
+}
