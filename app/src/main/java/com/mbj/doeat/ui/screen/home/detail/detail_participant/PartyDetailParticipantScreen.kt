4c4eab341a4a94aeb3a7bababf074911aa0f247b
meenjoon <zqudwnsz@naver.com>
2023-10-16 12:15:20 +0900

    feat: PartyDetailParticipantScreen 생성

---
+++ app/src/main/java/com/mbj/doeat/ui/screen/home/detail/detail_participant/PartyDetailParticipantScreen.kt
@@ -0,0 +1,10 @@
+package com.mbj.doeat.ui.screen.home.detail.detail_participant
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.mbj.doeat.data.remote.model.Party
+
+@Composable
+fun PartyDetailParticipantScreen(party: Party, navController: NavHostController, onClick: () -> Unit) {
+    
+}
