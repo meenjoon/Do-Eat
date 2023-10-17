c9812f4036a41402cb85bf818149d5a01293e4cc
meenjoon <zqudwnsz@naver.com>
2023-10-18 03:03:31 +0900

    feat: 채팅방 내부 제목 설정

--- app/src/main/java/com/mbj/doeat/ui/screen/home/chat_detail/ChatDetailScreen.kt
+++ app/src/main/java/com/mbj/doeat/ui/screen/home/chat_detail/ChatDetailScreen.kt
@@ -4,17 +4,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -51,6 +56,17 @@ fun ChatDetailScreen(postId: String, navController: NavHostController, onClick: 
                     modifier = Modifier.padding(8.dp),
                     navController = navController
                 )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                chatRoomItemState?.name?.let { name ->
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h5,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
 
             LazyColumn(
