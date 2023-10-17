6b534f88a8fe47bb08ebc2a1f01f220d1519eaee
meenjoon <zqudwnsz@naver.com>
2023-10-17 09:55:13 +0900

    chore: Firebase 사용을 위한 코드 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
         classpath("com.android.tools:r8:8.2.16-dev")
         classpath ("org.jetbrains.kotlin:kotlin-serialization:1.4.0")
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
 
