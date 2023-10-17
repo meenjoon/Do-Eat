6ea553a7471c67cfa64f1b449ad261fc47321c07
meenjoon <zqudwnsz@naver.com>
2023-10-18 01:16:37 +0900

    feat: ChatRoom 모델 생성

---
+++ app/src/main/java/com/mbj/doeat/data/remote/model/ChatRoom.kt
@@ -0,0 +1,14 @@
+package com.mbj.doeat.data.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ChatRoom(
+    @SerialName("postId") val postId: String? = null,
+    @SerialName("name") val name: String? = null,
+    @SerialName("createdChatRoomDate") val createdChatRoomDate: String? = null,
+    @SerialName("members") val members: Map<String, Boolean>? = null,
+    @SerialName("lastMessage") val lastMessage: String? = null,
+)
+
