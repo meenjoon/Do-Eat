d303e201c8030e8e57dcd2734b20235de55844e1
meenjoon <zqudwnsz@naver.com>
2023-10-18 19:04:39 +0900

    feat: 파티 개설자 화면에 채팅하기 UI 및 관련 로직 구현

--- app/src/main/java/com/mbj/doeat/ui/screen/home/detail/detail_writer/viewmodel/PartyDetailWriterViewModel.kt
+++ app/src/main/java/com/mbj/doeat/ui/screen/home/detail/detail_writer/viewmodel/PartyDetailWriterViewModel.kt
@@ -3,11 +3,17 @@ package com.mbj.doeat.ui.screen.home.detail.detail_writer.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
+import com.mbj.doeat.data.remote.model.ChatRoom
 import com.mbj.doeat.data.remote.model.Party
 import com.mbj.doeat.data.remote.model.PartyPostIdRequestDto
 import com.mbj.doeat.data.remote.network.adapter.ApiResultSuccess
+import com.mbj.doeat.data.remote.network.api.chat_db.repository.ChatDBRepository
 import com.mbj.doeat.data.remote.network.api.default_db.repository.DefaultDBRepository
 import com.mbj.doeat.ui.graph.BottomBarScreen
+import com.mbj.doeat.ui.graph.DetailScreen
+import com.mbj.doeat.util.DateUtils
+import com.mbj.doeat.util.NavigationUtils
+import com.mbj.doeat.util.UserDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,10 +22,16 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PartyDetailWriterViewModel @Inject constructor(private val defaultDBRepository: DefaultDBRepository) : ViewModel() {
+class PartyDetailWriterViewModel @Inject constructor(
+    private val defaultDBRepository: DefaultDBRepository,
+    private val chatDBRepository: ChatDBRepository
+) : ViewModel() {
 
     private val _partyItem = MutableStateFlow<Party?>(null)
     val partyItem: StateFlow<Party?> = _partyItem
+
+    private val _chatRoomItem = MutableStateFlow<ChatRoom?>(null)
+    val chatRoomItem: StateFlow<ChatRoom?> = _chatRoomItem
 
     private val _showDeletePartyDialog = MutableStateFlow<Boolean>(false)
     val showDeletePartyDialog: StateFlow<Boolean> = _showDeletePartyDialog
@@ -27,12 +39,40 @@ class PartyDetailWriterViewModel @Inject constructor(private val defaultDBReposi
     private val _isLoadingView = MutableStateFlow<Boolean>(false)
     val isLoadingView: StateFlow<Boolean> = _isLoadingView
 
+    init {
+        getChatRoomItem()
+    }
+
     fun updateSearchItem(inputPartyItem: Party) {
         _partyItem.value = inputPartyItem
     }
 
     fun changeShowDeletePartyDialog(showDialog: Boolean) {
         _showDeletePartyDialog.value = showDialog
+    }
+
+    fun enterChatRoom(navController: NavHostController) {
+        viewModelScope.launch {
+            val myUserInfo = UserDataStore.getLoginResponse()
+
+            chatDBRepository.enterChatRoom(
+                onComplete = { },
+                onError = { },
+                postId = partyItem.value?.postId.toString(),
+                postUserId = partyItem.value?.userId.toString(),
+                myUserId = myUserInfo?.userId.toString(),
+                restaurantName = partyItem.value?.restaurantName!!,
+                createdChatRoom = DateUtils.getCurrentTime()
+            ).collectLatest { response ->
+                if (response is ApiResultSuccess) {
+                    NavigationUtils.navigate(
+                        navController, DetailScreen.ChatDetail.navigateWithArg(
+                            partyItem.value?.postId.toString()
+                        )
+                    )
+                }
+            }
+        }
     }
 
     fun deleteParty(navHostController: NavHostController) {
@@ -48,6 +88,23 @@ class PartyDetailWriterViewModel @Inject constructor(private val defaultDBReposi
                         popUpTo(navHostController.graph.id) {
                             inclusive = true
                         }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getChatRoomItem() {
+        viewModelScope.launch {
+            partyItem.collectLatest { partyItem ->
+                if (partyItem != null) {
+                    chatDBRepository.getChatRoomItem(
+                        onComplete = { },
+                        onError = { },
+                        postId = partyItem.postId.toString()
+
+                    ) { chatRoomItem ->
+                        _chatRoomItem.value = chatRoomItem
                     }
                 }
             }
